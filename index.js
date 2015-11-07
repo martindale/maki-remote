00c582a52d0b21098b201e610a5dab0aaf4269bb
Eric Martindale <eric@ericmartindale.com>
2015-11-07 01:17:01 -0800

    Add support for `update` method.

--- index.js
+++ index.js
@@ -33,8 +33,31 @@ Remote.prototype._post = function(url, data, cb) {
   });
 };
 
+Remote.prototype._patch = function(url, data, cb) {
+  var self = this;
+  if (typeof data === 'function') {
+    cb = data;
+    data = {};
+  }
+  rest.patch( this.url + url , {
+    headers: {
+      'Accept': 'application/json'
+    },
+    data: data
+  }).on('complete', function(data) {
+    if (!data[ self.id ]) return cb(data);
+    return cb(null, data);
+  });
+};
+
 Remote.prototype.create = function(obj, cb) {
   this._post('', obj, cb);
+};
+
+Remote.prototype.update = function(query, params, cb) {
+  // TODO: make both queries and paths work with this
+  // probably pass everything to '' as query params
+  this._patch('/' + query, params, cb);
 };
 
 Remote.prototype.query = function(obj, cb) {
