85dccfc234f9d38e2daec67e1c517607a43fcef1
Eric Martindale <eric@ericmartindale.com>
2016-06-29 22:45:33 +0200

    Add basic support for events.

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
+var url = require('url');
+var util = require('util');
 var rest = require('restler');
+var WebSocket = require('ws');
 
-function Remote(url, id) {
-  this.url = url;
-  this.id = id || '_id';
+var RPCService = require('./lib/service-rpc');
+
+function Remote(base, id) {
+  this.url = base;
+  this.id = id || 'id';
+
+  var parsed = url.parse(base);
+
+  this.host = parsed.host;
+  this.path = parsed.path;
+
+  this.resourceMap = {};
 }
+
+util.inherits(Remote, require('events').EventEmitter);
+
+Remote.prototype.init = function(cb) {
+  var self = this;
+  if (!cb) var cb = new Function();
+
+  self._options('/', function(err, options) {
+    self.resources = options.resources;
+
+    options.resources.forEach(function(r) {
+      self.resourceMap[r.name] = r;
+    });
+
+    var endpoint = 'ws://' + self.host + self.path;
+
+    self.ws = new WebSocket(endpoint);
+    self.ws.on('open', self.emit.bind(self, 'open'));
+    self.ws.on('message', function(msg) {
+      self.emit('message', JSON.parse(msg));
+    });
+
+    cb();
+  });
+};
+
+Remote.prototype._options = function(url, cb) {
+  var self = this;
+  rest.request( this.url + url , {
+    method: 'OPTIONS',
+    parser: rest.parsers.json,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).on('complete', function(data) {
+    return cb(null, data);
+  });
+};
 
 Remote.prototype._get = function(url, cb) {
   var self = this;
@@ -24,6 +74,26 @@ Remote.prototype._post = function(url, data, cb) {
     data = {};
   }
   rest.postJson( this.url + url , data, {
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).on('complete', function(data) {
+    if (!data[ self.id ]) return cb(data);
+    return cb(null, data);
+  });
+};
+
+Remote.prototype._put = function(url, data, cb) {
+  var self = this;
+  if (typeof data === 'function') {
+    cb = data;
+    data = {};
+  }
+
+  var endpoint = this.url + url;
+  console.log('_put endpoint:', endpoint);
+
+  rest.putJson(endpoint, data, {
     headers: {
       'Accept': 'application/json'
     }
